496c0118d09d23dc06e731ef406f9d16e8ddc9e5
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-12-29 16:44:34 +0530

    add quikfix list and term keybinds

--- init.lua
+++ init.lua
@@ -1,14 +1,34 @@
 require("config.lazy")
 
 vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.keymap.set("n", "<Space>%", "<cmd>source %<CR>")
 vim.keymap.set("n", "<Space>x", ":.lua<CR>")
 vim.keymap.set("v", "<Space>x", ":lua<CR>")
 vim.keymap.set("n", "<Space>w", "<C-W>")
+vim.keymap.set("n", "cn", ":cnext<CR>")
+vim.keymap.set("n", "cp", ":cprev<CR>")
+
+vim.keymap.set("n", "<Space>ot", function()
+  vim.cmd.vnew()
+  vim.cmd.term("zsh")
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+end)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking",
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function() vim.highlight.on_yank() end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+    vim.cmd.startinsert()
+  end,
 })
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -15,15 +15,15 @@ return {
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
 
-          if not client then return end
+          if not c then return end
 
-          if client:supports_method('textDocument/formatting') then
+          if c:supports_method('textDocument/formatting') then
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = args.buf,
               callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
               end,
             })
           end
