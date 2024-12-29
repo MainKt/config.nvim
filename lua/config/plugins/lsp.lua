12f4e6267fbf2c1a7a4d0c60b70cdf0503f40576
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-12-29 23:43:41 +0530

    add scala metals lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,47 +1,72 @@
 return {
-  "neovim/nvim-lspconfig",
-  lazy = false,
-  dependencies = {
-    'saghen/blink.cmp',
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      'saghen/blink.cmp',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
+      { "williamboman/mason.nvim", opts = {} },
+      "williamboman/mason-lspconfig.nvim",
     },
-    { "williamboman/mason.nvim", opts = {} },
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    require("mason-lspconfig").setup {
-      handlers = {
-        function(server)
-          require("lspconfig")[server].setup { capabilities = capabilities }
-        end
+      require("mason-lspconfig").setup {
+        handlers = {
+          function(server)
+            require("lspconfig")[server].setup { capabilities = capabilities }
+          end
+        }
       }
-    }
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(args)
-        local c = vim.lsp.get_client_by_id(args.data.client_id)
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
 
-        if not c then return end
+          if not c then return end
 
-        if c:supports_method('textDocument/formatting') then
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = args.buf,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-            end,
-          })
-        end
-      end,
-    })
-  end,
-  keys = { { "<space>=", vim.lsp.buf.format } },
+          if c:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+    keys = { { "<space>=", vim.lsp.buf.format } },
+  },
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.capabilities = require('blink.cmp').get_lsp_capabilities()
+      metals_config.on_attach = function(client, bufnr) end
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
+  }
 }
